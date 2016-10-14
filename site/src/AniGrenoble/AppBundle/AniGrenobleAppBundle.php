1099f880e67e82212137f045aaf553851333553b
alice caporali <alice.caporali@hotmail.fr>
2016-10-14 15:32:32 +0200

    Initialisation du projet symfony2.8 + bundle aniGrenoble

---
+++ site/src/AniGrenoble/AppBundle/AniGrenobleAppBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace AniGrenoble\AppBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AniGrenobleAppBundle extends Bundle
+{
+}
---
+++ site/src/AniGrenoble/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AniGrenoble\AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('AniGrenobleAppBundle:Default:index.html.twig');
+    }
+}
