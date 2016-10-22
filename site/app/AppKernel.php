72300614d892c2bbeb35ba19ea1a9046a4074d87
alice caporali <alice.caporali@hotmail.fr>
2016-10-22 23:17:30 +0200

    Continu la mise en page + modi navbarre
    modification de la navbarre (new bar pour les droits des utilisateurs
    connectés),background, changement d'element en css

--- site/app/AppKernel.php
+++ site/app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new AniGrenoble\AppBundle\AniGrenobleAppBundle(),
             new AniGrenoble\UtilisateurBundle\AniGrenobleUtilisateurBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
--- site/src/AniGrenoble/AppBundle/Controller/DefaultController.php
+++ site/src/AniGrenoble/AppBundle/Controller/DefaultController.php
@@ -29,10 +29,15 @@ class DefaultController extends Controller
             ));
     }
 
-    public function viewAction()
+    public function a_proposAction()
+    {
+        return $this->render('AniGrenobleAppBundle:Default:a_propos.html.twig');
+    }
+
+    /*public function viewAction()
     {
         return $this->render('AniGrenobleAppBundle:Default:view.html.twig');
-    }
+    }*/
 
     /**
      * @Security("has_role('ROLE_ADMIN')")
@@ -72,47 +77,44 @@ class DefaultController extends Controller
             
     }
 
-    public function editAction()
+    public function editAction($id, Request $request)
     {
-        $advert = $this->getDoctrine()
+        $annonce = $this->getDoctrine()
             ->getManager()
             ->getRepository('AniGrenobleAppBundle:Annonce')
             ->find($id)
         ;
 
-        // On crée le FormBuilder grâce au service form factory
-        $form = $this->get('form.factory')->createBuilder('form', $annonce)
-            ->add('titre', 'text')
-            ->add('contenu', 'textarea')
-            ->add('auteur', 'text')
-            ->add('date', 'datetime')
-            ->add('publie', 'checkbox', array('required' => false))
-            ->add('save', 'submit')
-            ->getForm()
-        ;
+        // Si l'article n'existe pas, on affiche une erreur 404
+        if ($annonce === null) {
+            throw new NotFoundHttpException("L'annonce d'id ".$id." n'existe pas.");
+        }
+
+        // On crée le FormBuilder grâce au service form factory et avec les données récupérées dans la base de données
+        $form = $this->get('form.factory')->create(new AnnonceType, $annonce);
 
         // On fait le lien Requête <-> Formulaire
-        // À partir de maintenant, la variable $advert contient les valeurs entrées dans le formulaire par le visiteur
+        // À partir de maintenant, la variable $annonce contient les valeurs entrées dans le formulaire par le visiteur
         $form->handleRequest($request);
 
         // On vérifie que les valeurs entrées sont correctes
         // (Nous verrons la validation des objets en détail dans le prochain chapitre)
         if ($form->isValid()) {
-            // On l'enregistre notre objet $advert dans la base de données, par exemple
+            // On l'enregistre notre objet $annonce dans la base de données, par exemple
             $em = $this->getDoctrine()->getManager();
-            $em->persist($advert);
+            $em->persist($annonce);
             $em->flush();
 
             $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
 
-            // On redirige vers la page de visualisation de l'annonce nouvellement créée
-            return $this->redirect($this->generateUrl('oc_platform_view', array('id' => $advert->getId())));
+            // On redirige vers la page d'accueil'
+            return $this->redirect($this->generateUrl('ani_grenoble_app_homepage'));
         }
 
         // À ce stade, le formulaire n'est pas valide car :
         // - Soit la requête est de type GET, donc le visiteur vient d'arriver sur la page et veut voir le formulaire
         // - Soit la requête est de type POST, mais le formulaire contient des valeurs invalides, donc on l'affiche de nouveau
-        return $this->render('AniGrenobleAppBundle:Default:add.html.twig', array(
+        return $this->render('AniGrenobleAppBundle:Default:edit.html.twig', array(
           'form' => $form->createView(),
         ));
     }
