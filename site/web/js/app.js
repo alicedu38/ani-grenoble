ae7a00d4e4100a496e313fc21a68b0a7e6a97800
alice caporali <alice.caporali@hotmail.fr>
2016-10-25 12:31:30 +0200

    Retour à la ligne + css + mise en page contact
    Retour à la ligne pour le contenu d'annonce. Ajout de css (a, bouton,
    etc).Amélioration de la mise ne page de contact

--- site/web/js/app.js
+++ site/web/js/app.js
@@ -1,15 +1,16 @@
 $( document ).ready(function(){
     $(".button-collapse").sideNav(); //Barre menu mobile
     $('.materialboxed').materialbox();//Effet d'ombres hover les elements page index
+    $( "span" ).remove( ".sr-only" );//Suprrime span dans bar des droits
     
     width =$(document).width();//Responsive cadre evenement (horizontal ->vertical)
     if (width <= 600) {
         $(".div-evenement div.hoverable").attr('class', 'card vertical hoverable');
     };
 
-    if ($(".form")[0]){//Page form add/modifier annonce : echange pas position de deux elements pour creer checkbox
-        $('input.date-date').removeAttr("type");
-        $('input.date-date').prop('type', 'date');
-        $(".label_publie").before($(".checkbox_publie"));
+    if ($(".form")[0]){//Page form add/modifier annonce : 
+        $('input.date-date').removeAttr("type");//Suprrime l'attribut type
+        $('input.date-date').prop('type', 'date');//Recréer l'attibut type et prend pour valeur "date"
+        $(".label_publie").before($(".checkbox_publie"));//echange pas position de deux elements pour creer checkbox
     }
 })
