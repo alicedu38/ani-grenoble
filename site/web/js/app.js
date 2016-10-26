2599868ae8b9f7486dee2faad10436809619387b
alice caporali <alice.caporali@hotmail.fr>
2016-10-26 22:27:39 +0200

    Mise en page +renomme images

--- site/web/js/app.js
+++ site/web/js/app.js
@@ -1,19 +1,6 @@
 $( document ).ready(function(){
     $(".button-collapse").sideNav(); //Barre menu mobile
-    $('.materialboxed').materialbox();//Effet d'ombres hover les elements page index
-    $( "span" ).remove( ".sr-only" );//Suprrime span dans bar des droits utilisateurs
-    $( ".connexion form label:nth-child(7)" ).remove( "label" );//Suppriem remember me (se souvenir de moi) de form de connexion
-    
-    width =$(document).width();//Responsive cadre evenement (horizontal ->vertical)
-    if (width <= 600) {
-        $(".div-evenement div.hoverable").attr('class', 'card vertical hoverable');
-    };
-
-    if ($(".form")[0]){//Page form add/modifier annonce : 
-        $('input.date-date').removeAttr("type");//Suprrime l'attribut type
-        $('input.date-date').prop('type', 'date');//Recréer l'attibut type et prend pour valeur "date"
-        $(".label_publie").before($(".checkbox_publie"));//echange pas position de deux elements pour creer checkbox
-    }
+    $('.materialboxed').materialbox();//Effet d'ombres hover les elements page index 
 
     //Scroll to top
     //Check to see if the window is top if not then display button
@@ -30,4 +17,21 @@ $( document ).ready(function(){
         $('html, body').animate({scrollTop : 0},800);
         return false;
     });
+})
+
+$( document ).ready(function(){
+    $( "span" ).remove( ".sr-only" );//Suprrime span dans bar des droits utilisateurs
+    $( ".connexion form label:nth-child(7)" ).remove( "label" );//Supprime remember me (se souvenir de moi) de form de connexion
+    $('h2').after('<hr>');
+
+    width =$(document).width();//Responsive cadre evenement (horizontal to vertical)
+    if (width <= 600) {
+        $(".div-evenement div.hoverable").attr('class', 'card vertical hoverable');
+    };
+
+    if ($(".form")[0]){//Page form add/modifier annonce : 
+        $('input.date-date').removeAttr("type");//Suprrime l'attribut type
+        $('input.date-date').prop('type', 'date');//Recréer l'attibut type et prend pour valeur "date"
+        $(".label_publie").before($(".checkbox_publie"));//echange pas position de deux elements pour creer checkbox
+    }
 })
