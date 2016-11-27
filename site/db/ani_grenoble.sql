22dc2e0440eed462492da9813fec3ef05d55f66d
alice caporali <alice.caporali@hotmail.fr>
2016-11-27 12:20:20 +0100

    MAJ bd

--- site/db/ani_grenoble.sql
+++ site/db/ani_grenoble.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Lun 07 Novembre 2016 à 18:32
+-- Généré le :  Dim 27 Novembre 2016 à 12:19
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -36,18 +36,19 @@ CREATE TABLE IF NOT EXISTS `annonce` (
   `image_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UNIQ_F65593E53DA5256D` (`image_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=9 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=11 ;
 
 --
 -- Contenu de la table `annonce`
 --
 
 INSERT INTO `annonce` (`id`, `titre`, `contenu`, `auteur`, `date`, `publie`, `image_id`) VALUES
-(4, 'Réunion du 13/10/2016', 'Du coup, bilan de la réunion d''hier soir : \r\n- Handa kun\r\n- Keijo (ça je pense que tout le monde a retenu le titre XD)\r\n- Débat vite fait sur la 3D dans l''animation\r\n- Karaoké\r\n- Élections (de manière démocratique mdr) des membres du bureau, à savoir le Président Tom, le Vice-Président Maxime, le Trésorier Thibault et le Secrétaire Quentin', 'Alice', '2016-10-15', 1, 3),
-(5, 'Concours de dessin de la mascotte d''Ani-grenoble', 'ANNONCE IMPORTANTE : concours de dessin pour désigner la mascotte de l''association ANI Grenoble.\r\nMagnifique affiche faite par Maxime qui réalise aussi celles de l''assos et qui sont toutes aussi splendides :)', 'Alice', '2016-10-22', 1, 4),
+(4, 'Réunion du 13/10/2016', 'Du coup, bilan de la réunion d''hier soir : \n- Handa kun\n- Keijo (ça je pense que tout le monde a retenu le titre XD)\n- Débat vite fait sur la 3D dans l''animation\n- Karaoké\n- Élections (de manière démocratique mdr) des membres du bureau, à savoir le Président Tom, le Vice-Président Maxime, le Trésorier Thibault et le Secrétaire Quentin', 'Alice', '2016-10-15', 1, 3),
+(5, 'Concours de dessin de la mascotte d''Ani-grenoble', 'ANNONCE IMPORTANTE : concours de dessin pour désigner la mascotte de l''association ANI Grenoble.\r\nMagnifique affiche faite par Maxime qui réalise aussi celles de l''assos et qui sont toutes aussi splendides :)', 'Alice', '2016-10-22', 0, 4),
 (6, 'Réunion du 18/10/2016', 'Bilan de ce soir :\r\n- Un-go\r\n- Berserk (1997)\r\n- Présentation très complète sur Berserk par Tom \r\n- Blindtest\r\nN''oubliez pas de participer au concours de dessin ainsi que de cotiser pour ceux qui veulent :)', 'Alice', '2016-10-22', 1, 5),
-(7, 'Réunion du 20/09/2016', 'Bilan de la 1ère réunion qui s''est déroulée ce soir : \r\n- 1er ep de Danshi koukousei no nichijou\r\n- 1er ep de Black Lagoon\r\n- Blindtest normal\r\n- Quizz image\r\n- Time''s up\r\nDu coup, très bonne soirée en votre compagnie. Bien content qu''on soit plus d''une dizaine (espérons qu''on soit de plus en plus nombreux).', 'Alice', '2016-09-20', 1, 6),
-(8, 'Réunion du 26/10/2016', 'Bilan de la réunion de ce soir : \r\n- Rokka no yuusha\r\n- Thunderbolt fantasy\r\n- Présentation de Valentin sur le jeu Bloodborne', 'Alice', '2016-10-26', 1, 7);
+(7, 'Réunion du 20/09/2016', 'Bilan de la 1ère réunion qui s''est déroulée ce soir : \n- 1er ep de Danshi koukousei no nichijou\n- 1er ep de Black Lagoon\n- Blindtest normal\n- Quizz image\n- Time''s up\nDu coup, très bonne soirée en votre compagnie. Bien content qu''on soit plus d''une dizaine (espérons qu''on soit de plus en plus nombreux).', 'Alice', '2016-09-20', 1, 6),
+(8, 'Réunion du 26/10/2016', 'Bilan de la réunion de ce soir : \n- Rokka no yuusha\n- Thunderbolt fantasy\n- Présentation de Valentin sur le jeu Bloodborne', 'Alice', '2016-10-26', 1, 7),
+(10, 'Réunion du 14/11/2016', 'Bilan de la réunion : \n- 91 days\n- Yamada-kun\n- Présentation de Quentin sur la censure\n- Blindtest\nLes volontaires qui veulent bien nous aider à préparer la soirée sont les bienvenus.', 'Alice', '2016-11-14', 1, 8);
 
 -- --------------------------------------------------------
 
@@ -72,7 +73,8 @@ INSERT INTO `annonce_categorie` (`annonce_id`, `categorie_id`) VALUES
 (5, 1),
 (6, 2),
 (7, 2),
-(8, 2);
+(8, 2),
+(10, 2);
 
 -- --------------------------------------------------------
 
@@ -120,7 +122,7 @@ CREATE TABLE IF NOT EXISTS `image` (
   `url` longtext COLLATE utf8_unicode_ci,
   `alt` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=8 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=9 ;
 
 --
 -- Contenu de la table `image`
@@ -131,7 +133,8 @@ INSERT INTO `image` (`id`, `url`, `alt`) VALUES
 (4, 'uploads/img/ani-grenoble-concours-mascotte.jpg', 'mon image concours'),
 (5, 'uploads/img/berserk.jpg', 'berserk.jpg'),
 (6, 'uploads/img/Black lagoon.jpg', 'black lagoon'),
-(7, 'uploads/img/rokka-no-yuusha.jpg', 'Rokka no yuusha');
+(7, 'uploads/img/rokka-no-yuusha.jpg', 'Rokka no yuusha'),
+(8, 'uploads/img/91-days.jpg', '91 days');
 
 -- --------------------------------------------------------
 
@@ -169,7 +172,7 @@ CREATE TABLE IF NOT EXISTS `utilisateur` (
 
 INSERT INTO `utilisateur` (`id`, `username`, `username_canonical`, `email`, `email_canonical`, `enabled`, `salt`, `password`, `last_login`, `locked`, `expired`, `expires_at`, `confirmation_token`, `password_requested_at`, `roles`, `credentials_expired`, `credentials_expire_at`) VALUES
 (15, 'superadmin', 'superadmin', 'superadmin@superadmin.fr', 'superadmin@superadmin.fr', 1, 'cqpt6ht8bbww0840cggok4wg0gw8k4o', 'sLxPvXpv93sQpNiRuzNXJzir4dxvcuCZJXhGMK2uhCuCr0a9awsFNIhWV79I7/jMCG0IXmGycHWo+NX1EJmMDQ==', '2016-10-30 23:01:26', 0, 0, NULL, NULL, NULL, 'a:1:{i:0;s:16:"ROLE_SUPER_ADMIN";}', 0, NULL),
-(16, 'admin', 'admin', 'admin@admin.fr', 'admin@admin.fr', 1, 'q30kqwcase844ssokg00wc48o4ksoc0', 'mrk7DsH7S5KfdxUK+O5tVy4XeNr6wbZxzFfArW2ifvKe8HKSZ97eeQ7O/367wQ5xColMa6GZlKrfcz7XoG0H0g==', '2016-10-27 23:46:16', 0, 0, NULL, NULL, NULL, 'a:1:{i:0;s:10:"ROLE_ADMIN";}', 0, NULL);
+(16, 'admin', 'admin', 'admin@admin.fr', 'admin@admin.fr', 1, 'q30kqwcase844ssokg00wc48o4ksoc0', 'mrk7DsH7S5KfdxUK+O5tVy4XeNr6wbZxzFfArW2ifvKe8HKSZ97eeQ7O/367wQ5xColMa6GZlKrfcz7XoG0H0g==', '2016-11-25 22:54:38', 0, 0, NULL, NULL, NULL, 'a:1:{i:0;s:10:"ROLE_ADMIN";}', 0, NULL);
 
 --
 -- Contraintes pour les tables exportées
