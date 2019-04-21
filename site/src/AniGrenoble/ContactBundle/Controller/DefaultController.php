43bbe7e342675f2f4cc3859b4d8ee71994ca3277
alicecaporali <alice.caporali01@gmail.com>
2019-04-21 22:28:40 +0200

    2.8 to 3.4 and remove class for contact form

--- site/src/AniGrenoble/ContactBundle/Controller/DefaultController.php
+++ site/src/AniGrenoble/ContactBundle/Controller/DefaultController.php
@@ -4,34 +4,68 @@ namespace AniGrenoble\ContactBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use AniGrenoble\ContactBundle\Entity\Enquiry;
-use AniGrenoble\ContactBundle\Form\EnquiryType;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class DefaultController extends Controller
 {
   public function contactAction(Request $request)
   {
-    $enquiry = new Enquiry();
-    $form = $this->createForm(new EnquiryType(), $enquiry);
+    $form = $this->createFormBuilder()
+        ->add('name', TextType::class, [
+            'label' => 'Nom',
+            'required' => false,
+        ])
+        ->add('email', EmailType::class, [
+            'label' => 'Email *',
+            'required'=> true
+        ])
+        ->add('title', TextType::class, [
+            'label' => 'Sujet',
+            'required' => false,
+        ])
+        ->add('message', TextareaType::class, [
+            'label' => 'Message *',
+            'required'=> true
+        ])
+        ->add('send', SubmitType::class, [
+            'label' => 'Envoyer',
+            'attr' => [
+                'class' => 'btn btn-primary'
+            ]
+        ])
+        ->getForm();
 
-    $request = $this->getRequest();
-    if ($request->getMethod() == 'POST') {
-      $form->handleRequest($request);
+    $form->handleRequest($request);
 
-      //Si le formulaire de contact est valide alors envoie du mail
-      if ($form->isValid()) {
+    //Si le formulaire de contact est valide alors envoie du mail
+    if ($form->isSubmitted() && $form->isValid()) {
         $message = \Swift_Message::newInstance()
+        ->setFrom('send@example.com')
         ->setSubject('Mail du site ANI Grenoble')
-        ->setFrom('send@hotmail.fr')
         ->setTo($this->container->getParameter('blogger_blog.emails.contact_email'))
-        ->setBody($this->renderView('AniGrenobleContactBundle:Default:contactEmail.txt.twig', array('enquiry' => $enquiry)));
-        $this->get('mailer')->send($message);
+        ->setBody(
+            $this->renderView(
+                // app/Resources/views/Emails/registration.html.twig
+                'AniGrenobleContactBundle:Default:contactEmail.txt.twig',
+                array(
+                    'name' => $form->get('name')->getData(),
+                    'email' => $form->get('email')->getData(),
+                    'title' => $form->get('title')->getData(),
+                    'message' => $form->get('message')->getData(),
+                )
+            )
+        );
 
+        $this->get('mailer')->send($message);
         $request->getSession()->getFlashBag()->add('notice green accent-2', 'Message envoyé avec succès ! Merci.');
 
         return $this->redirect($this->generateUrl('ani_grenoble_contact_contact'));
-      }
     }
+
 
     return $this->render('AniGrenobleContactBundle:Default:contact.html.twig', array(
       'form' => $form->createView()
