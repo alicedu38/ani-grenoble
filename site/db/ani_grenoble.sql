69c91e5ea6cbd0c640752f647d17797552d16998
ACaporali <alice.caporali01@gmail.com>
2019-04-22 14:54:08 +0200

    Change sql file

--- site/db/ani_grenoble.sql
+++ site/db/ani_grenoble.sql
@@ -1,20 +1,22 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
--- http://www.phpmyadmin.net
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
 --
--- Client :  127.0.0.1
--- Généré le :  Dim 27 Novembre 2016 à 12:19
--- Version du serveur :  5.6.17
--- Version de PHP :  5.5.12
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  lun. 22 avr. 2019 à 12:52
+-- Version du serveur :  5.7.21
+-- Version de PHP :  7.0.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de données :  `ani_grenoble`
@@ -26,6 +28,7 @@ SET time_zone = "+00:00";
 -- Structure de la table `annonce`
 --
 
+DROP TABLE IF EXISTS `annonce`;
 CREATE TABLE IF NOT EXISTS `annonce` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `titre` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
@@ -36,17 +39,17 @@ CREATE TABLE IF NOT EXISTS `annonce` (
   `image_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UNIQ_F65593E53DA5256D` (`image_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=11 ;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Contenu de la table `annonce`
+-- Déchargement des données de la table `annonce`
 --
 
 INSERT INTO `annonce` (`id`, `titre`, `contenu`, `auteur`, `date`, `publie`, `image_id`) VALUES
-(4, 'Réunion du 13/10/2016', 'Du coup, bilan de la réunion d''hier soir : \n- Handa kun\n- Keijo (ça je pense que tout le monde a retenu le titre XD)\n- Débat vite fait sur la 3D dans l''animation\n- Karaoké\n- Élections (de manière démocratique mdr) des membres du bureau, à savoir le Président Tom, le Vice-Président Maxime, le Trésorier Thibault et le Secrétaire Quentin', 'Alice', '2016-10-15', 1, 3),
-(5, 'Concours de dessin de la mascotte d''Ani-grenoble', 'ANNONCE IMPORTANTE : concours de dessin pour désigner la mascotte de l''association ANI Grenoble.\r\nMagnifique affiche faite par Maxime qui réalise aussi celles de l''assos et qui sont toutes aussi splendides :)', 'Alice', '2016-10-22', 0, 4),
-(6, 'Réunion du 18/10/2016', 'Bilan de ce soir :\r\n- Un-go\r\n- Berserk (1997)\r\n- Présentation très complète sur Berserk par Tom \r\n- Blindtest\r\nN''oubliez pas de participer au concours de dessin ainsi que de cotiser pour ceux qui veulent :)', 'Alice', '2016-10-22', 1, 5),
-(7, 'Réunion du 20/09/2016', 'Bilan de la 1ère réunion qui s''est déroulée ce soir : \n- 1er ep de Danshi koukousei no nichijou\n- 1er ep de Black Lagoon\n- Blindtest normal\n- Quizz image\n- Time''s up\nDu coup, très bonne soirée en votre compagnie. Bien content qu''on soit plus d''une dizaine (espérons qu''on soit de plus en plus nombreux).', 'Alice', '2016-09-20', 1, 6),
+(4, 'Réunion du 13/10/2016', 'Du coup, bilan de la réunion d\'hier soir : \n- Handa kun\n- Keijo (ça je pense que tout le monde a retenu le titre XD)\n- Débat vite fait sur la 3D dans l\'animation\n- Karaoké\n- Élections (de manière démocratique mdr) des membres du bureau, à savoir le Président Tom, le Vice-Président Maxime, le Trésorier Thibault et le Secrétaire Quentin', 'Alice', '2016-10-15', 1, 3),
+(5, 'Concours de dessin de la mascotte d\'Ani-grenoble', 'ANNONCE IMPORTANTE : concours de dessin pour désigner la mascotte de l\'association ANI Grenoble.\r\nMagnifique affiche faite par Maxime qui réalise aussi celles de l\'assos et qui sont toutes aussi splendides :)', 'Alice', '2016-10-22', 0, 4),
+(6, 'Réunion du 18/10/2016', 'Bilan de ce soir :\r\n- Un-go\r\n- Berserk (1997)\r\n- Présentation très complète sur Berserk par Tom \r\n- Blindtest\r\nN\'oubliez pas de participer au concours de dessin ainsi que de cotiser pour ceux qui veulent :)', 'Alice', '2016-10-22', 1, 5),
+(7, 'Réunion du 20/09/2016', 'Bilan de la 1ère réunion qui s\'est déroulée ce soir : \n- 1er ep de Danshi koukousei no nichijou\n- 1er ep de Black Lagoon\n- Blindtest normal\n- Quizz image\n- Time\'s up\nDu coup, très bonne soirée en votre compagnie. Bien content qu\'on soit plus d\'une dizaine (espérons qu\'on soit de plus en plus nombreux).', 'Alice', '2016-09-20', 1, 6),
 (8, 'Réunion du 26/10/2016', 'Bilan de la réunion de ce soir : \n- Rokka no yuusha\n- Thunderbolt fantasy\n- Présentation de Valentin sur le jeu Bloodborne', 'Alice', '2016-10-26', 1, 7),
 (10, 'Réunion du 14/11/2016', 'Bilan de la réunion : \n- 91 days\n- Yamada-kun\n- Présentation de Quentin sur la censure\n- Blindtest\nLes volontaires qui veulent bien nous aider à préparer la soirée sont les bienvenus.', 'Alice', '2016-11-14', 1, 8);
 
@@ -56,6 +59,7 @@ INSERT INTO `annonce` (`id`, `titre`, `contenu`, `auteur`, `date`, `publie`, `im
 -- Structure de la table `annonce_categorie`
 --
 
+DROP TABLE IF EXISTS `annonce_categorie`;
 CREATE TABLE IF NOT EXISTS `annonce_categorie` (
   `annonce_id` int(11) NOT NULL,
   `categorie_id` int(11) NOT NULL,
@@ -65,7 +69,7 @@ CREATE TABLE IF NOT EXISTS `annonce_categorie` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Contenu de la table `annonce_categorie`
+-- Déchargement des données de la table `annonce_categorie`
 --
 
 INSERT INTO `annonce_categorie` (`annonce_id`, `categorie_id`) VALUES
@@ -82,14 +86,15 @@ INSERT INTO `annonce_categorie` (`annonce_id`, `categorie_id`) VALUES
 -- Structure de la table `categorie`
 --
 
+DROP TABLE IF EXISTS `categorie`;
 CREATE TABLE IF NOT EXISTS `categorie` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Contenu de la table `categorie`
+-- Déchargement des données de la table `categorie`
 --
 
 INSERT INTO `categorie` (`id`, `nom`) VALUES
@@ -99,42 +104,30 @@ INSERT INTO `categorie` (`id`, `nom`) VALUES
 -- --------------------------------------------------------
 
 --
--- Structure de la table `enquiry`
---
-
-CREATE TABLE IF NOT EXISTS `enquiry` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nom` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `sujet` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `corps` longtext COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
 -- Structure de la table `image`
 --
 
+DROP TABLE IF EXISTS `image`;
 CREATE TABLE IF NOT EXISTS `image` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `url` longtext COLLATE utf8_unicode_ci,
   `alt` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=9 ;
+  `categorie_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_C53D045FBCF5E72D` (`categorie_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Contenu de la table `image`
+-- Déchargement des données de la table `image`
 --
 
-INSERT INTO `image` (`id`, `url`, `alt`) VALUES
-(3, 'uploads/img/handa-kun.jpg', '5sports.png'),
-(4, 'uploads/img/ani-grenoble-concours-mascotte.jpg', 'mon image concours'),
-(5, 'uploads/img/berserk.jpg', 'berserk.jpg'),
-(6, 'uploads/img/Black lagoon.jpg', 'black lagoon'),
-(7, 'uploads/img/rokka-no-yuusha.jpg', 'Rokka no yuusha'),
-(8, 'uploads/img/91-days.jpg', '91 days');
+INSERT INTO `image` (`id`, `url`, `alt`, `categorie_id`) VALUES
+(3, 'uploads/img/handa-kun.jpg', '5sports.png', NULL),
+(4, 'uploads/img/ani-grenoble-concours-mascotte.jpg', 'mon image concours', NULL),
+(5, 'uploads/img/berserk.jpg', 'berserk.jpg', NULL),
+(6, 'uploads/img/Black lagoon.jpg', 'black lagoon', NULL),
+(7, 'uploads/img/rokka-no-yuusha.jpg', 'Rokka no yuusha', NULL),
+(8, 'uploads/img/91-days.jpg', '91 days', NULL);
 
 -- --------------------------------------------------------
 
@@ -142,6 +135,7 @@ INSERT INTO `image` (`id`, `url`, `alt`) VALUES
 -- Structure de la table `utilisateur`
 --
 
+DROP TABLE IF EXISTS `utilisateur`;
 CREATE TABLE IF NOT EXISTS `utilisateur` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(180) COLLATE utf8_unicode_ci NOT NULL,
@@ -149,33 +143,28 @@ CREATE TABLE IF NOT EXISTS `utilisateur` (
   `email` varchar(180) COLLATE utf8_unicode_ci NOT NULL,
   `email_canonical` varchar(180) COLLATE utf8_unicode_ci NOT NULL,
   `enabled` tinyint(1) NOT NULL,
-  `salt` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `salt` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `last_login` datetime DEFAULT NULL,
-  `locked` tinyint(1) NOT NULL,
-  `expired` tinyint(1) NOT NULL,
-  `expires_at` datetime DEFAULT NULL,
   `confirmation_token` varchar(180) COLLATE utf8_unicode_ci DEFAULT NULL,
   `password_requested_at` datetime DEFAULT NULL,
   `roles` longtext COLLATE utf8_unicode_ci NOT NULL COMMENT '(DC2Type:array)',
-  `credentials_expired` tinyint(1) NOT NULL,
-  `credentials_expire_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UNIQ_1D1C63B392FC23A8` (`username_canonical`),
   UNIQUE KEY `UNIQ_1D1C63B3A0D96FBF` (`email_canonical`),
   UNIQUE KEY `UNIQ_1D1C63B3C05FB297` (`confirmation_token`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=17 ;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Contenu de la table `utilisateur`
+-- Déchargement des données de la table `utilisateur`
 --
 
-INSERT INTO `utilisateur` (`id`, `username`, `username_canonical`, `email`, `email_canonical`, `enabled`, `salt`, `password`, `last_login`, `locked`, `expired`, `expires_at`, `confirmation_token`, `password_requested_at`, `roles`, `credentials_expired`, `credentials_expire_at`) VALUES
-(15, 'superadmin', 'superadmin', 'superadmin@superadmin.fr', 'superadmin@superadmin.fr', 1, 'cqpt6ht8bbww0840cggok4wg0gw8k4o', 'sLxPvXpv93sQpNiRuzNXJzir4dxvcuCZJXhGMK2uhCuCr0a9awsFNIhWV79I7/jMCG0IXmGycHWo+NX1EJmMDQ==', '2016-10-30 23:01:26', 0, 0, NULL, NULL, NULL, 'a:1:{i:0;s:16:"ROLE_SUPER_ADMIN";}', 0, NULL),
-(16, 'admin', 'admin', 'admin@admin.fr', 'admin@admin.fr', 1, 'q30kqwcase844ssokg00wc48o4ksoc0', 'mrk7DsH7S5KfdxUK+O5tVy4XeNr6wbZxzFfArW2ifvKe8HKSZ97eeQ7O/367wQ5xColMa6GZlKrfcz7XoG0H0g==', '2016-11-25 22:54:38', 0, 0, NULL, NULL, NULL, 'a:1:{i:0;s:10:"ROLE_ADMIN";}', 0, NULL);
+INSERT INTO `utilisateur` (`id`, `username`, `username_canonical`, `email`, `email_canonical`, `enabled`, `salt`, `password`, `last_login`, `confirmation_token`, `password_requested_at`, `roles`) VALUES
+(15, 'superadmin', 'superadmin', 'superadmin@superadmin.fr', 'superadmin@superadmin.fr', 1, 'ccJmCY3Si0Hzb8N4BuPAWIActp8fotF.49EYbn7KsWo', 'CVkevskPOul5tMgdtKLtTa9bUD/zJhR7ROpdocwzL4+ZZXsl7SI4H8IveJ9biFPoAPkWTYaUNMKSRXs/0SdSvw==', '2019-04-22 12:40:14', NULL, NULL, 'a:1:{i:0;s:16:\"ROLE_SUPER_ADMIN\";}'),
+(16, 'admin', 'admin', 'admin@admin.fr', 'admin@admin.fr', 1, 'sUjUhvOceh8lYKKYtVFQsN6MRZO1EbRjJB5Ltj7zKSw', 't9rFq6H2ukAUjUyhpD3+LqHKXf2XZ17wvEqKiKgORw9quOpnEbehG2voPlFcYXyerS7sM149OcvVOj1gCyAmiw==', '2019-04-22 12:41:11', NULL, NULL, 'a:1:{i:0;s:10:\"ROLE_ADMIN\";}');
 
 --
--- Contraintes pour les tables exportées
+-- Contraintes pour les tables déchargées
 --
 
 --
@@ -190,6 +179,13 @@ ALTER TABLE `annonce`
 ALTER TABLE `annonce_categorie`
   ADD CONSTRAINT `FK_3C5A3DA68805AB2F` FOREIGN KEY (`annonce_id`) REFERENCES `annonce` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `FK_3C5A3DA6BCF5E72D` FOREIGN KEY (`categorie_id`) REFERENCES `categorie` (`id`) ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `image`
+--
+ALTER TABLE `image`
+  ADD CONSTRAINT `FK_C53D045FBCF5E72D` FOREIGN KEY (`categorie_id`) REFERENCES `categorie` (`id`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
